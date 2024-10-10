ac8f3b1d49e587a8577f80ff903c77f38fdb3bb5
irsanwoy <irsanwoy54@gmail.com>
2024-10-11 02:03:59 +0700

    tutorial flutter petanikode finish

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,34 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  // Fungsi untuk menambah counter
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      title: 'First App',
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Aplikasi Flutter Petani Kode'),
+            backgroundColor: Colors.deepOrange,
+          ),
+          body:  Column(
+              children: <Widget>[
+                Image.network('https://www.petanikode.com/img/flutter/flutter.png'),
+                Text(
+                  'Belajar Flutter untuk Pemula',
+                  style: TextStyle(fontSize: 24, fontFamily: "Serif", height: 2.0),
+                ),
+                Text('oleh Petani Kode'),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Image.network('https://www.petanikode.com/img/flutter/flutter-sqr.png'),
+                    Image.network('https://www.petanikode.com/img/flutter/flutter-sqr.png')
+                  ],
+                )
+            ]
+          )
       ),
     );
   }
